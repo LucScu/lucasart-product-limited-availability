753fb9a29921891529b8f1efdfef5c2610e56f05
Luca Sculco <lugos84@gmail.com>
2016-11-26 18:45:09 +0100

    fix to module name

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'LucasArt_StockAvailability',
+    'LucasArt_StockStatusLabel',
     __DIR__
 );
